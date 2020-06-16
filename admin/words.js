d6d95168ff76a8f3fca14ec5f4d8d11d9b45817b
phillipssc <sean.c.phillips@gmail.com>
2020-06-16 21:31:23 +0000

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'rtl_433 adapter settings': {
+        'en': 'Adapter settings for rtl_433',
+        'de': 'Adaptereinstellungen für rtl_433',
+        'ru': 'Настройки адаптера для rtl_433',
+        'pt': 'Configurações do adaptador para rtl_433',
+        'nl': 'Adapterinstellingen voor rtl_433',
+        'fr': "Paramètres d'adaptateur pour rtl_433",
+        'it': "Impostazioni dell'adattatore per rtl_433",
+        'es': 'Ajustes del adaptador para rtl_433',
+        'pl': 'Ustawienia adaptera dla rtl_433',
+        'zh-cn': 'rtl_433的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'option1',
+        'ru': 'option1',
+        'pt': 'option1',
+        'nl': 'option1',
+        'fr': 'option1',
+        'it': 'option1',
+        'es': 'option1',
+        'pl': 'option1',
+        'zh-cn': 'option1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'option2',
+        'ru': 'option2',
+        'pt': 'option2',
+        'nl': 'option2',
+        'fr': 'option2',
+        'it': 'option2',
+        'es': 'option2',
+        'pl': 'option2',
+        'zh-cn': 'option2'
+    }
+};
